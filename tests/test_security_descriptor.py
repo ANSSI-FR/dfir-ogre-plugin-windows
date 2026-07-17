eb3336da56fda84ec049566103c28a35d39135ad
Adrien Salais <adrien.salais@ssi.gouv.fr>
2026-07-17 13:41:34 +0200

    Pluralize scheduled task security descriptor ACEs

--- tests/test_security_descriptor.py
+++ tests/test_security_descriptor.py
@@ -1,10 +1,46 @@
 import json
 from unittest import TestCase
 
-from dfir_ogre_plugin_windows.security_descriptor import ACE
+from dfir_ogre_plugin_windows.security_descriptor import ACE, SecurityDescriptor
 
 
 class SecurityDescriptorTest(TestCase):
+    def test_security_descriptor_uses_plural_ace_arrays(self):
+        dacl_descriptor = SecurityDescriptor()
+        dacl_descriptor.from_string("O:SYG:BAD:(A;;KR;;;BU)")
+
+        dacl_record = json.loads(dacl_descriptor.to_record().to_string())
+
+        self.assertEqual(
+            dacl_record["dacl_aces"],
+            [
+                {
+                    "ace_type": "ACCESS_ALLOWED_ACE_TYPE",
+                    "account_sid": "DOMAIN_ALIAS_RID_USERS",
+                    "rights": ["KEY_READ"],
+                }
+            ],
+        )
+        self.assertNotIn("dacl_ace", dacl_record)
+
+        sacl_descriptor = SecurityDescriptor()
+        sacl_descriptor.from_string("S:(AU;SA;KR;;;WD)")
+
+        sacl_record = json.loads(sacl_descriptor.to_record().to_string())
+
+        self.assertEqual(
+            sacl_record["sacl_aces"],
+            [
+                {
+                    "ace_type": "SYSTEM_AUDIT_ACE_TYPE",
+                    "account_sid": "SECURITY_WORLD_RID",
+                    "ace_flags": ["SUCCESSFUL_ACCESS_ACE_FLAG"],
+                    "rights": ["KEY_READ"],
+                }
+            ],
+        )
+        self.assertNotIn("sacl_ace", sacl_record)
+
     def test_object_ace_guids_are_lowercase(self):
         ace = ACE()
         ace.from_string(
